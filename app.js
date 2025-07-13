b62392ac98f10abf0225e4c75a95443b5df5def0
bhuvan <bhuvanannappa@gmail.com>
2025-07-13 11:37:43 +0530

    setup path for static files

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const auth = require("./routes/auth");
 const gadget = require("./routes/gadget");
+const path = require("path");
 const isLoggedIn = require("./middlewares/isLoggedIn");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
 
 app.use("/api", auth);
 app.use("/api/gadgets", isLoggedIn, gadget);
